5707ee970d453587fab2a7bb7104ec585939f467
Het Modha <hdmodha15@gmail.com>
2022-05-03 16:22:22 +0530

    initialize

---
+++ score_board.py
@@ -0,0 +1,38 @@
+from turtle import Turtle
+from typing import TextIO
+
+ALIGN = "center"
+FONT = ("Courier", 20, "normal")
+
+
+class ScoreBoard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(-10, 260)
+        self.hideturtle()
+        with open("data.txt", mode="r") as file:
+            self.high_score = int(file.read())
+        self.update_score()
+
+    def update_score(self):
+        self.clear()
+        self.write(f"Score = {self.score} High Score: {self.high_score}", move=False, align=ALIGN, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as file:
+                file.write(str(self.high_score))
+        self.score = 0
+        self.update_score()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", move=False, align=ALIGN, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.update_score()
